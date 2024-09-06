76148431d52263683ff45c31edad695eeab1ba66
tabish-10 <muhammadtabishfarooqi069@gmail.com>
2024-09-06 15:21:50 -0700

    updat

---
+++ main.py
@@ -0,0 +1,11 @@
+name:str = "Mohammad Tabish"
+print(name)
+
+
+father_name:str = "Abdul Malik"
+print(father_name)
+
+name:str = "Mohammad Tabish "
+print( type(name) )
+print( id(name))
+print( dir(name))
